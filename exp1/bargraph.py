3874267bbce67ba3698bc2ce7531118a93359123
lordbecerril <becerrilblas@gmail.com>
2020-08-27 20:45:18 -0700

    Graphs for experiment

--- exp1/bargraph.py
+++ exp1/bargraph.py
@@ -1,17 +1,27 @@
 import matplotlib.pyplot as plt
-%matplotlib inline
-plt.style.use('ggplot')
 
-x = [2,3,5,3,5,4]
-energy = [5, 6, 15, 22, 24, 8]
 
-x_pos = [i for i, _ in enumerate(x)]
 
-plt.bar(x_pos, energy, color='green')
-plt.xlabel("Energy Source")
-plt.ylabel("Energy Output (GJ)")
-plt.title("Energy output from various fuel sources")
+def graphmaker(x_axis, y_axis, title, y_label):
 
-plt.xticks(x, x_pos)
+    fig = plt.figure() # This line is needed to create a figure
 
-plt.show()
+    ax = fig.add_axes([0,0,1,1]) # This makes a 2d bar graph
+
+    ax.set_ylabel(y_label)
+
+    ax.set_title(title)
+
+
+    ax.bar(x_axis,y_axis) # the .bar() creates the bar graph
+
+    plt.show() # finally this command, shows the plot
+
+
+
+print("6 second stuff")
+title = 'Counts with voltage set at 900V'
+x_axis = ['0','1','2','3','4','5','6','7','8','9','10','11'] # here we have what we want on our x axis
+y_axis = [6,10,38,36,42,25,20,16,4,1,1,1] # and here we have what we want on our y axis
+y_label = 'cnts/0.1 min'
+graphmaker(x_axis, y_axis, title, y_label)
