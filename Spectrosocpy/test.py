a3ec93a6f7dedb2b81f918d1f26a95e54d257601
lordbecerril <becerrilblas@gmail.com>
2020-09-23 10:44:15 -0700

    Attempts at finding R added

--- Spectrosocpy/test.py
+++ Spectrosocpy/test.py
@@ -9,6 +9,12 @@ from matplotlib import pyplot as plt
 def grating_func(x, a, b):
     return a * (np.sin(np.radians(x-b))-np.sin(np.radians(b))) # Fitting a and b to grating equation
 
+#def r_func(x, a):
+#    return( (4 * np.power(x,2))/(a*(np.power(x,2) - 4)))
+
+def r_func(x, a):
+    return( a * ((1/4) - ( 1/ np.power(x,2))))
+
 
 
 x = [14.808, 19.475, 20.679, 20.665, 32.083 ,41.112 ,44.271 ,44.525] #Averaged Angles we gathered
@@ -41,6 +47,30 @@ print("Theta i is ", theta_i)
 plt.figure(figsize=(6, 4))
 plt.scatter(x, y, label='Data')
 plt.plot(x, grating_func(x, param[0], param[1]),
+         label='Fitted function')
+plt.legend(loc='best')
+#plt.show()
+plt.clf()
+
+
+
+#hydrogen_angles = [15.25, 17.245, 23.837, 31.784, 35.992, 52.492]
+hydrogen_angles = [ 31.784, 35.992, 52.492]
+
+
+calc_lambda_h = [param[0]*(np.sin(np.radians(theta_m-theta_i))-np.sin(np.radians(theta_i))) for theta_m in hydrogen_angles]
+calc_lambda_h =[1/l for l in calc_lambda_h]
+print("Calculated Wavelengths are ", calc_lambda_h)
+
+n_vals = [6, 4, 3]
+
+param, param_cov = curve_fit(r_func, n_vals, calc_lambda_h, p0 = None)
+print("R is", param*10**3)
+
+# Create the plot
+plt.figure(figsize=(6, 4))
+plt.scatter(n_vals, calc_lambda_h, label='Data')
+plt.plot(n_vals, r_func(n_vals, param[0]),
          label='Fitted function')
 plt.legend(loc='best')
 plt.show()
