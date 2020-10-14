b4be28c78d66d93c7e2daececa2ffd015b1dc950
lordbecerril <becerrilblas@gmail.com>
2020-10-13 18:15:05 -0700

    Finished script

--- SpeedOfLight/grapher.py
+++ SpeedOfLight/grapher.py
@@ -5,6 +5,7 @@ from scipy.optimize import curve_fit
 from matplotlib import pyplot as plt
 from scipy import stats
 
+print("In Air")
 
 y = [1,2,3,4,5,6] # Meters
 x = [0.6E-9, 4.4E-9, 7.3E-9, 10.6E-9,13.8E-9,17.6E-9] #rising edges
@@ -18,8 +19,53 @@ print("Standard Error is ", std_err)
 #plt.plot(x,y,  'o', label='original data')
 plt.plot(x, [intercept + slope * n for n in x], 'r', label='fitted line')
 plt.errorbar(x,y,yerr=[1.5,0.5,0.2,0.2,0.1,0.2], fmt = 'o')
-plt.title("Linear Fit of Rising Edge")
+plt.title("Linear Fit of Rising Edge in Air")
 plt.ylabel("Distance")
 plt.xlabel("Time")
 plt.legend()
 plt.show()
+plt.clf()
+
+
+
+
+print("In pipe")
+
+y = [4,5,5,6,6] # Meters
+x = [7.6E-9, 3E-9, 4.6E-9, 16.5E-9, 17.8E-9] #rising edges
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+print("Slope is ", slope)
+print("Intercept is ", intercept)
+print("R value is ", r_value)
+print("P value is ", p_value)
+print("Standard Error is ", std_err)
+#plt.plot(x,y,  'o', label='original data')
+plt.plot(x, [intercept + slope * n for n in x], 'r', label='fitted line')
+plt.errorbar(x,y,yerr=[2.6, 5.3, 4.3, 0.3, 2.6], fmt = 'o')
+plt.title("Linear Fit of Rising Edge in Pipe")
+plt.ylabel("Distance")
+plt.xlabel("Time")
+plt.legend()
+plt.show()
+plt.clf()
+
+print("In water")
+y = [4,5,6] # Meters
+x = [12.6E-9,14.6E-9,20.2E-9] #rising edges
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+print("Slope is ", slope)
+print("Intercept is ", intercept)
+print("R value is ", r_value)
+print("P value is ", p_value)
+print("Standard Error is ", std_err)
+#plt.plot(x,y,  'o', label='original data')
+plt.plot(x, [intercept + slope * n for n in x], 'r', label='fitted line')
+plt.errorbar(x,y,yerr=[1.5,2.5,0.9], fmt = 'o')
+plt.title("Linear Fit of Rising Edge in Water")
+plt.ylabel("Distance")
+plt.xlabel("Time")
+plt.legend()
+plt.show()
+plt.clf()
