b6bba2874186bfa032a3a4a6fc5ad1d069c95d8d
lordbecerril <becerrilblas@gmail.com>
2020-10-15 16:55:27 -0700

    BLACKBODY

--- SpeedOfLight/grapher.py
+++ SpeedOfLight/grapher.py
@@ -23,7 +23,8 @@ plt.title("Linear Fit of Rising Edge in Air")
 plt.ylabel("Distance")
 plt.xlabel("Time")
 plt.legend()
-plt.show()
+#plt.show()
+plt.savefig("./inair.png")
 plt.clf()
 
 
@@ -47,12 +48,80 @@ plt.title("Linear Fit of Rising Edge in Pipe")
 plt.ylabel("Distance")
 plt.xlabel("Time")
 plt.legend()
-plt.show()
+#plt.show()
+plt.savefig("./inpipe.png")
+
 plt.clf()
 
 print("In water")
 y = [4,5,6] # Meters
 x = [12.6E-9,14.6E-9,20.2E-9] #rising edges
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+print("Slope is ", slope)
+print("Intercept is ", intercept)
+print("R value is ", r_value)
+print("P value is ", p_value)
+print("Standard Error is ", std_err)
+#plt.plot(x,y,  'o', label='original data')
+plt.plot(x, [intercept + slope * n for n in x], 'r', label='fitted line')
+plt.errorbar(x,y,yerr=[1.5,2.5,0.9], fmt = 'o')
+plt.title("Linear Fit of Rising Edge in Water")
+plt.ylabel("Distance")
+plt.xlabel("Time")
+plt.legend()
+#plt.show()
+plt.savefig("./inwater.png")
+
+plt.clf()
+
+
+print("Averages from Air")
+
+y = [1,2,3,4,5,6] # Meters
+x = [2.1E-9, 4.0E-9, 7.3E-9, 10.6E-9,13.7E-9,17.6E-9] #rising edges
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+print("Slope is ", slope)
+print("Intercept is ", intercept)
+print("R value is ", r_value)
+print("P value is ", p_value)
+print("Standard Error is ", std_err)
+#plt.plot(x,y,  'o', label='original data')
+plt.plot(x, [intercept + slope * n for n in x], 'r', label='fitted line')
+plt.errorbar(x,y,yerr=[1.5,0.5,0.2,0.2,0.1,0.2], fmt = 'o')
+plt.title("Linear Fit of Rising Edge in Air")
+plt.ylabel("Distance")
+plt.xlabel("Time")
+plt.legend()
+plt.show()
+plt.clf()
+
+print("Averages from Pipe")
+
+y = [4,5,5,6,6] # Meters
+x = [10.2E-9, 8.3E-9, 8.3E-9, 16.3E-9, 15.2E-9] #rising edges
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+print("Slope is ", slope)
+print("Intercept is ", intercept)
+print("R value is ", r_value)
+print("P value is ", p_value)
+print("Standard Error is ", std_err)
+#plt.plot(x,y,  'o', label='original data')
+plt.plot(x, [intercept + slope * n for n in x], 'r', label='fitted line')
+plt.errorbar(x,y,yerr=[2.6, 5.3, 4.3, 0.3, 2.6], fmt = 'o')
+plt.title("Linear Fit of Rising Edge in Pipe")
+plt.ylabel("Distance")
+plt.xlabel("Time")
+plt.legend()
+plt.show()
+plt.clf()
+
+
+print("Averages from water")
+y = [4,5,6] # Meters
+x = [14.1E-9,17.1E-9,21.1E-9] #rising edges
 
 slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
 print("Slope is ", slope)
---
+++ blackbody/data_analysis.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("data.csv") # Read data
+
+
+# Calculate Resistance
+res =[]
+for i in range(len(df.index)):
+    r = df.iloc[i,0]/df.iloc[i,1]
+    res.append(r)
+df['Resistance']=res
+
+
+# Calculate power below with P = I^2*R
+power = []
+for i in range(len(df.index)):
+    x = (df.iloc[i,1])**2  * (df.iloc[i,2])
+    power.append( x)
+
+# Append power to Dataframe
+df['Power'] = power
+#print(df)
+
+
+ratio = 1.68E-9 # The ratio of len over area from R = RHO * (len/Cross sectional Area)
+
+resistivity = []
+for i in range(len(df.index)):
+    x = df.iloc[i,2] * ratio
+    resistivity.append(x)
+
+df['Resistivity'] = resistivity
+#print(df)
+
+# Using RHOt = RHOo[2 + a(T-To)] solve for T and plug in calced resistivites
+temperature = []
+for i in range(len(df.index)):
+    T = df.iloc[i,4] / (5.64E-8)
+    T = T-1
+    T = T / 0.0045
+    T = T + 26.85
+    T = T+273.15
+    temperature.append(T)
+#print(temperature)
+df['Temperature'] = temperature
+
+
+print(df)
